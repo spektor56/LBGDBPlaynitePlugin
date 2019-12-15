042d2573fe5061e63d97604efa2091226030f2a1
Brett Sanderson <l-brett-l@hotmail.com>
2019-12-15 01:09:37 -0500

    work

---
+++ LBGDBMetadata/LbgdbMetadataSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Playnite.SDK;
+
+namespace LBGDBMetadata
+{
+    public class LbgdbMetadataSettings : ObservableObject, ISettings
+    {
+        public void BeginEdit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndEdit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CancelEdit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool VerifySettings(out List<string> errors)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

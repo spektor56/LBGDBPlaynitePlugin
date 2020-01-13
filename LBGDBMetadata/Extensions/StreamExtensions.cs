e9131a744bb59f05c010eeee295baf52d3923b95
Brett Sanderson <l-brett-l@hotmail.com>
2020-01-13 13:17:24 -0500

    more work

---
+++ LBGDBMetadata/Extensions/StreamExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LBGDBMetadata.Extensions
+{
+    public static class StreamExtensions
+    {
+        public static IEnumerable<XElement> AsEnumerableXml(this Stream metaDataStream, string element)
+        {
+            using (var reader = XmlReader.Create(metaDataStream))
+            {
+                reader.MoveToContent();
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            if (reader.Name == element)
+                            {
+                                if (XNode.ReadFrom(reader) is XElement el)
+                                {
+                                    yield return el;
+                                }
+                            }
+
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TESTform/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using LBGDBMetadata;
+using LBGDBMetadata.Extensions;
+using LBGDBMetadata.LaunchBox.Metadata;
+
+namespace TESTform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var zipFile = @"C:\zipTest\MetaData.zip";
+
+            using (var zipArchive = ZipFile.Open(zipFile, ZipArchiveMode.Read))
+            {
+                var metaData = zipArchive.Entries.FirstOrDefault(entry =>
+                    entry.Name.Equals("MetaData.xml", StringComparison.OrdinalIgnoreCase));
+
+                if (metaData != null)
+                    using (var metaDataStream = metaData.Open())
+                    {
+                        var games = metaDataStream.AsEnumerableXml("Game");
+                        var xmlSerializer = new XmlSerializer(typeof(Game));
+
+                        var i = 0;
+                        var context = new MetaDataContext();
+                        context.ChangeTracker.AutoDetectChangesEnabled = false;
+
+                        foreach (var xElement in games)
+                        {
+                            var gameMetaData = (Game) xmlSerializer.Deserialize(xElement.CreateReader());
+                            i++;
+                            if (i++ > 1000)
+                            {
+                                context.SaveChanges();
+                                i = 0;
+                                context.Dispose();
+                                context = new MetaDataContext();
+                                context.ChangeTracker.AutoDetectChangesEnabled = false;
+                            }
+
+                            context.Games.Add(gameMetaData);
+                        }
+
+                        context.Dispose();
+                    }
+            }
+        }
+    }
+}

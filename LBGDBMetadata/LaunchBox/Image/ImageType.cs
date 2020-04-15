97f67359078cffdf56d9d9bf56d956ce1e65ea82
Brett Sanderson <l-brett-l@hotmail.com>
2020-04-14 22:36:08 -0400

    clear logo

--- LBGDBMetadata/LaunchBox/Image/ImageType.cs
+++ LBGDBMetadata/LaunchBox/Image/ImageType.cs
@@ -4,7 +4,7 @@ namespace LBGDBMetadata.LaunchBox.Image
 {
     public static class ImageType
     {
-        public static HashSet<string> Cover = new HashSet<string>() { "Box - Front", "Box - Front - Reconstructed", "Fanart - Box - Front"};
+        public static HashSet<string> Cover = new HashSet<string>() { "Clear Logo" };
         public static HashSet<string> Background = new HashSet<string>() {"Fanart - Background"};
     }
 }
